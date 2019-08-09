0e31eee4764ba549c2f36196ef637bb842e7f865
mik90 <mik90@pitt.edu>
2019-08-08 21:41:07 -0400

    got to 9.2

--- functionalCpp/ch_9.cpp
+++ functionalCpp/ch_9.cpp
@@ -139,6 +139,162 @@ void counting_finished()
 
 
 #endif
+
+// Sum types for error handling
+// Can have option with error code, std::exception_ptr,
+// or anything else
+
+// Simple error return, requires manual constructing
+// and destructing of T and E. Also should make functions
+// for both T and E
+template<typename T, typename E>
+class expected
+{
+  private:
+    union {
+      T m_value;
+      E m_error;
+    };
+    bool m_valid;
+    T& get()
+    {
+      if (!m_valid)
+        throw std::logic_error("Missing a value");
+      return m_value;
+    }
+    E& error()
+    {
+      if (m_valid)
+        throw std::logic_error("There is no error");
+      return m_error;
+    }
+    template <typename... Args>
+    static expected success(Args&&... params)
+    {
+      expected result;
+      result.m_valid = true;
+      // Init the value of type T in the memory location of
+      // m_value. Forward over the params of type Args.
+      // This uses already-allocated memory and constructs
+      // an object inside of it.
+      new (&result.m_value) T(std::forward<Args>(params)...);
+      return result;
+    }
+    template <typename... Args>
+    static expected error(Args&&... params)
+    {
+      expected result;
+      result.m_valid = false;
+      // Do the same for type E
+      new (&result.m_value) E(std::forward<Args>(params)...);
+      return result;
+    }
+    ~expected()
+    {
+      // Call the appropriate destructor
+      if (m_valid)
+        m_value.~T();
+      else
+        m_error.~E();
+    }
+    // Copy constructor
+    expected(const expected& other) : m_valid(other.m_valid)
+    {
+      // Construct 'other' in our union
+      if (m_valid)
+        new (&m_value) T(other.m_value);
+      else
+        new (&m_error) E(other.m_error);
+    }
+    // Move constructor
+    expected(expected&& other) : m_valid(other.m_valid)
+    {
+      // Construct 'other' in our union
+      if (m_valid)
+        new (&m_value) T(std::move(other.m_value));
+      else
+        new (&m_error) E(std::move(other.m_error));
+    }
+    // Implement the assignment operator with the copy-and-swap idiom
+    void swap(expected& other)
+    {
+      using std::swap;
+      if (m_valid)
+      {
+        if (other.m_valid)
+        {
+          // Both contain valid values, so swap
+          swap(m_value, other.m_value);
+        }
+        else
+        {
+          // This value is valid, other is an error
+          // Store error in temp variable
+          auto temp = std::move(other.m_error);
+          other.m_error.~E();
+          // Move our value into 'other'
+          new (&other.m_value) T(std::move(m_value));
+          // Then destroy it
+          m_value.~T();
+          // Move the temp error in our union
+          new (&m_error) E(std::move(temp));
+          // Swap our valid bool with other's
+          std::swap(m_valid, other.m_valid);
+        }
+      }
+      else
+      {
+        if (other.m_valid)
+        {
+          // This contains an error, other is valid.
+          // Use the previous case as the implementation
+          // Where 'this' is valid, other is error
+          other.swap(*this);
+        }
+        else
+        {
+          // Both are errors, just swap error values
+          swap(m_error, other.m_error);
+        }
+      }
+    }
+    // Assignment operator
+    expected& operator= (expected other)
+    {
+      // Use our swap method to swap
+      swap(other);
+      return *this;
+    }
+    // Casting operator for bool
+    operator bool() const
+    {
+      return m_valid;
+    }
+    // Casting operator for std::optional
+    operator std::optional<T>() const
+    {
+      if (m_valid)
+        return m_value;
+      else
+        // If the code uses optional, just
+        // omit the error in place of nothingness
+        return std::optional<T>();
+    }
+    // Implement get_if. Error type is just std::string
+    template <typename R, typename Variant,
+             typename Expected = expected<T, std::string>>
+    expected get_if(const Variant& variant)
+    {
+      R* ptr = std::get_if<R>(variant);
+
+      if (ptr)
+        return Expected::success(*ptr);
+      else
+        return Expected::error("Variant doesn't contain the expected type");
+    }
+};
+
+
 int main(int argc, char** argv)
 {
 
