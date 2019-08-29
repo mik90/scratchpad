8701e291645da866db8a532134930055ba0c1fbe
mik90 <kaliman.mike@gmail.com>
2019-08-29 18:07:06 -0400

    more 12

--- functionalCpp/ch_12.cpp
+++ functionalCpp/ch_12.cpp
@@ -2,6 +2,8 @@
 #include <functional>
 #include <numeric>
 #include <utility>
+#include <boost/asio.hpp>
+#include <json/json.h>
 
 // Ch 12 Funcitonal design for concurrent systems
 // Instead of having immutable data that is shared,
@@ -42,6 +44,108 @@ class Actor
 // Sinks: actors that just receive messages
 // Sources: actors that just send messages
 // General actors: actors that do both
+
+// Creating a simple message source
+// Bookmark service
+// Service - gets input from multiple sources and emits
+// those messages as a unified stream of data
+
+class service
+{
+  private:
+    tcp::acceptor m_acceptor;
+    tcp::socket   m_socket;
+    std::function<void(std::string&&)> m_emit;
+
+    void do_accept()
+    {
+      // We can accept multiple clients. We must be able to add them
+      m_acceptor.async_accept(m_socket,
+          [this] (const error_code& error)
+          {
+            if (!error)
+              // Creates a new session for the accepte dclient
+              make_shared_session(std::move(m_socket), m_emit)->start();
+            else
+              std::cerr << error.message() << std::endl;
+            do_accept();
+          });
+
+    }
+  public:
+    using value_type = std::string;
+
+    explicit service(boost::asio::io_service& service,
+                     unsigned short port = 42042)
+      : m_acceptor(service, tcp::endpoints(tcp::v4(), port)),
+        m_socket(service);
+    {
+    }
+    // Disable the copy constructor
+    service(const service& other) = delete;
+    // Use the default move constructor
+    service(service&& other) = default;
+
+    template <typename EmitFunction>
+    void set_message_handler(EmitFunction emit)
+    {
+      m_emit = emit;
+      // Only accept once we've been given a message handler
+      do_accept();
+    }
+};
+
+// A session should keep its own lifetime. If there's an error and the client
+// disconnects, the session should destroy itslef.
+// Session object can inherit from std::enable_shared_from_this.
+// This allows the session to create a shared pointer on itself whenever a client
+// connects. Once no clients are connected, it'll desctruct.
+
+template <typename EmitFunction>
+class session : public std::enable_shared_from_this<session<EmitFunction>>
+{
+  private:
+    tcp::socket m_socket;
+    boost::asio::streambuf m_data;
+    EmitFunction m_emit;
+
+
+    using shared_session =
+      std::enable_shared_from_this<session<EmitFunction>>;
+    void do_read()
+    {
+      // Creates another pointer to self
+      auto self = shared_session::shared_from_this();
+      // Schedules lambda to be executed when we get to a newline
+      // in the input
+      boost::asio::async_read_until(m_socket, m_data, '\n',
+          [this, self] (const error_code& error, std::size_t size)
+          {
+            if (!error)
+            {
+              // Read the line and send the line to whoever registered
+              // to listen for messages
+              std::istream is(&m_data);
+              std::string line;
+              std::getline(is,line);
+              m_emit(std::move(line));
+              do_read();
+            }
+          });
+    }
+
+  public:
+    session(tcp::socket&& socket, EmitFunction emit)
+      : m_socket(std::move(socket)), m_emit(emit)
+    {
+    }
+
+    void start()
+    {
+      do_read();
+    }
+};
+
 
 
 int main(int argc, char** argv)
